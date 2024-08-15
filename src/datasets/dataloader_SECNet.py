547ffdee0c73dd09972d7561db6a2895e8f41eb7
MA YING <72863547+mayingwuhu@users.noreply.github.com>
2024-08-15 11:39:25 +0800

    Update dataloader_SECNet.py

--- src/datasets/dataloader_SECNet.py
+++ src/datasets/dataloader_SECNet.py
@@ -1,9 +1,3 @@
-"""
-modified from UNITER codebase
-
-A meta data loader for sampling from different datasets / training tasks
-A prefetch loader to speedup data loading
-"""
 import random
 import sys
 
@@ -104,7 +98,7 @@ class PrefetchLoader(object):
             is_tuple = isinstance(batch, tuple)
             if is_tuple:
                 task, batch = batch
-            # LOGGER.info("输出tensor维度:", batch["visual_inputs_WH1"].size())
+           
             batch["visual_inputs_WH1"] = batch["visual_inputs_WH1"].float()
             batch["visual_inputs_WH2"] = batch["visual_inputs_WH2"].float()
             batch["visual_inputs_Where"] = batch["visual_inputs_Where"].float()
@@ -140,24 +134,10 @@ class PrefetchLoader(object):
         except StopIteration:
             self.batch = None
             return
-        # if record_stream() doesn't work, another option is to make sure
-        # device inputs are created on the main stream.
-        # self.next_input_gpu = torch.empty_like(self.next_input,
-        #                                        device='cuda')
-        # self.next_target_gpu = torch.empty_like(self.next_target,
-        #                                         device='cuda')
-        # Need to make sure the memory allocated for next_* is not still in use
-        # by the main stream at the time we start copying to next_*:
-        # self.stream.wait_stream(torch.cuda.current_stream())
+       
         with torch.cuda.stream(self.stream):
             self.batch = move_to_cuda(self.batch)
-            # more code for the alternative if record_stream() doesn't work:
-            # copy_ will record the use of the pinned source tensor in this
-            # side stream.
-            # self.next_input_gpu.copy_(self.next_input, non_blocking=True)
-            # self.next_target_gpu.copy_(self.next_target, non_blocking=True)
-            # self.next_input = self.next_input_gpu
-            # self.next_target = self.next_target_gpu
+            
 
     def next(self, it):
         torch.cuda.current_stream().wait_stream(self.stream)
